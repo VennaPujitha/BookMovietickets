9d7da897f1f99974a9b7e774762bb0a86a886f6d
Venna Pujitha <venkateswarluvenna700@gmail.com>
2025-10-26 12:03:56 +0530

    Add files via upload

---
+++ movie.py
@@ -0,0 +1,14 @@
+class Movie:
+    def __init__(self,movie_name,aval_tickets,ticket_price):
+        self.movie_name=movie_name
+        self.available_tickets=aval_tickets
+        self.ticket_price=ticket_price
+
+  
+ 
+
+
+
+
+
+ 
